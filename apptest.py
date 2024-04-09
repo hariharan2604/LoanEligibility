ac6c06da9db391bf92d67eb5529b46cd30d75a75
shariharan182003 <shariharan182003@gmail.com>
2024-04-09 12:21:45 +0530

    changes

--- apptest.py
+++ apptest.py
@@ -1,3 +1,4 @@
+import socket
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,6 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the host's IP address dynamically
+    host_ip = socket.gethostbyname(socket.gethostname())
+    app.run(host=host_ip, debug=True)
